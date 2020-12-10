b636faa8dae62523882b60ff40ecf5266701181b
kenny575 <75075053+kenny575@users.noreply.github.com>
2020-12-10 09:21:03 -0400

    Add files via upload

---
+++ prj_lf_c/inc/client.h
@@ -0,0 +1,41 @@
+/********************************************************/
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QDialog>
+#include <QDataStream>
+#include <QLocalSocket>
+
+QT_BEGIN_NAMESPACE
+class QLabel;
+class QLineEdit;
+class QPushButton;
+QT_END_NAMESPACE
+
+class Client : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Client(QWidget *parent = nullptr);
+
+private slots:
+    void requestNewFortune();
+    void readFortune();
+    void displayError(QLocalSocket::LocalSocketError socketError);
+    void enableGetFortuneButton();
+
+private:
+    QLineEdit *hostLineEdit;
+    QPushButton *getFortuneButton;
+    QLabel *statusLabel;
+
+    QLocalSocket *socket;
+    QDataStream in;
+    quint32 blockSize;
+
+    QString currentFortune;
+};
+
+#endif
